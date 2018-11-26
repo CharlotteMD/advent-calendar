3321a6e85aa7cc763aa59f7f747eea7011d2c39b
CharlotteMD <charlottemdavies@hotmail.co.uk>
2018-11-26 17:55:22 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  revealPresent = () => {
+      console.log('click');
+      this.toggle = true;
+  };
+
   render() {
+
+    const advent = [
+      {
+        text: 'First day of Advent',
+        present: 'Santa',
+        id: 'santa'
+      },{
+        text: 'Second day of Advent',
+        present: 'Present',
+        id: 'present'
+      },{
+        text: 'Third day of Advent',
+        present: 'Lights',
+        id: 'lights'
+      },{
+        text: 'Fourth day of Advent',
+        present: 'Cracker',
+        id: 'cracker'
+      },{
+        text: 'Fifth day of Advent',
+        present: 'Misletoe',
+        id: 'misletoe'
+      }
+      // },{
+      //   text: 'Sixth day of Advent',
+      //   present: 'Candy Cane'
+      // },{
+      //   text: 'Seventh day of Advent',
+      //   present: 'Carol'
+      // },{
+      //   text: 'Eighth day of Advent',
+      //   present: 'Stocking'
+      // },{
+      //   text: 'Ninth day of Advent',
+      //   present: 'Turkey'
+      // },{
+      //   text: 'Tenth day of Advent',
+      //   present: 'Shepherd'
+      // }
+    ];
+
+
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Advent 2018</h1>
         </header>
+
+        <div className="calendar">
+          {advent.map((day, i) =>
+            <div>
+              <p key={i} className="door" id={day.id}>{day.text}<span> {day.present}</span></p>
+              <button key={`button-${i}`} onClick={this.revealPresent}>Open</button>
+            </div>
+        )}
+        </div>
       </div>
     );
   }
