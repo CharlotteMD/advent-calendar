e6d2482e71a3dae81565862bb19279018757aa9d
CharlotteMD <charlottemdavies@hotmail.co.uk>
2018-12-17 12:53:19 +0000

    added all images locally

--- src/App.js
+++ src/App.js
@@ -4,6 +4,43 @@ import './App.css';
 
 import Card from './components/card.js'
 
+import image1 from './images/background/lights.jpeg';
+import image2 from './images/background/dog.jpeg';
+import image3 from './images/background/presents.jpeg';
+import image4 from './images/background/bauble.jpeg';
+import image5 from './images/background/tree.jpeg';
+import image6 from './images/background/cookies.jpeg';
+import image7 from './images/background/lantern.jpeg';
+import image8 from './images/background/reindeer.jpeg';
+import image9 from './images/background/town.jpeg';
+import image10 from './images/background/wreath.jpeg';
+
+import dog1 from './images/days/1.jpg';
+import dog2 from './images/days/2.jpg';
+import dog3 from './images/days/3.jpg';
+import dog4 from './images/days/4.jpg';
+import dog5 from './images/days/5.jpg';
+import dog6 from './images/days/6.jpg';
+import dog7 from './images/days/7.jpg';
+import dog8 from './images/days/8.png';
+import dog9 from './images/days/9.jpg';
+import dog10 from './images/days/10.jpg';
+import dog11 from './images/days/11.jpg';
+import dog12 from './images/days/12.jpg';
+import dog13 from './images/days/13.jpg';
+import dog14 from './images/days/14.jpg';
+import dog15 from './images/days/15.jpg';
+import dog16 from './images/days/16.jpg';
+import dog17 from './images/days/17.jpg';
+import dog18 from './images/days/18.jpg';
+import dog19 from './images/days/19.jpg';
+import dog20 from './images/days/20.jpg';
+import dog21 from './images/days/21.jpg';
+import dog22 from './images/days/22.jpg';
+import dog23 from './images/days/23.jpg';
+import dog24 from './images/days/24.jpg';
+
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -40,9 +77,16 @@ class App extends Component {
 
   changeBackground = () => {
     var images = [
-      'https://images.unsplash.com/photo-1513639725746-c5d3e861f32a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-      'https://images.unsplash.com/photo-1513885535751-8b9238bd345a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-      'https://images.unsplash.com/photo-1482097623218-813ddc8d7046?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80'
+      `${image1}`,
+      `${image2}`,
+      `${image3}`,
+      `${image4}`,
+      `${image5}`,
+      `${image6}`,
+      `${image7}`,
+      `${image8}`,
+      `${image9}`,
+      `${image10}`
     ];
 
     var generatedNumber = (Math.ceil(Math.random()*(images.length))-1);
@@ -64,99 +108,99 @@ class App extends Component {
       {
         day: 19,
         caption: 'Admire your Christmas decorations',
-        img: 'https://i.pinimg.com/564x/24/2e/ae/242eae83587d953b727b9defb49f5d88.jpg'
+        img: `${dog19}`
       },{
         day: 16,
         caption: 'Get your friends together',
-        img: 'https://i.pinimg.com/564x/35/a9/0a/35a90a00dc55c0e9ff34ea891dc22269.jpg'
+        img: `${dog16}`
       },{
         day: 18,
         caption: 'Visit Father Christmas',
-        img: 'https://www.maidenhead-advertiser.co.uk/resizer/748/503/false/GN4_DAT_2756716.jpg--.jpg?1479211583000'
+        img: `${dog18}`
       },{
         day: 21,
         caption: 'Be extra good so you are definitely on the nice list!',
-        img: 'https://i.pinimg.com/564x/33/09/c4/3309c44a8e61904b899b8763bec4ed9b.jpg'
+        img: `${dog21}`
       },{
         day: 5,
         caption: 'Make a Christmas wreath',
-        img: 'https://i.pinimg.com/564x/b5/1c/ee/b51cee713faf4f37b99ad3a02404c395.jpg'
+        img: `${dog5}`
       },{
         day: 20,
         caption: 'See the family',
-        img: 'https://i.pinimg.com/564x/b1/36/9d/b1369d3333a4a7802dd3f59b2e5700d8.jpg'
+        img: `${dog20}`
       },{
         day: 17,
         caption: 'Build a snowman',
-        img: 'https://i.pinimg.com/564x/fe/f4/09/fef409d251179b16d21565aced258581.jpg'
+        img: `${dog17}`
       },{
         day: 24,
         caption: 'Hang your stocking',
-        img: 'https://i.pinimg.com/564x/e7/f8/fc/e7f8fcb795e6d5cf7622f5725bd53684.jpg'
+        img: `${dog24}`
       },{
         day: 7,
         caption: 'Send out your Christmas list',
-        img: 'https://www.animaltalk.com.au/wp-content/uploads/2016/12/bigstock-A-beautiful-Havanese-aka-Javan-158318408.jpg'
+        img: `${dog7}`
       },{
         day: 4,
         caption: 'Write your Christmas cards',
-        img: 'https://www.clenthillsvets.co.uk/wp-content/uploads/2017/11/clent-hills-vets-christmas-sausage-dog.jpg'
+        img: `${dog4}`
       },{
         day: 8,
         caption: 'Indulge in plenty of festive snacks!',
-        img: 'https://www.skinnerspetfoods.co.uk/wp-content/uploads/2015/12/christmas-pup11-300x250.png'
+        img: `${dog8}`
       },{
         day: 1,
         caption: 'Hang your Christmas wreath',
-        img: 'https://i.pinimg.com/564x/74/99/85/749985451239391229b845276da53fb9.jpg'
+        img: `${dog1}`
       },{
         day: 3,
         caption: 'Get out the Christmas decorations',
-        img: 'https://i.pinimg.com/564x/df/aa/77/dfaa779359aedf748702bb7c087de5e0.jpg'
+        img: `${dog3}`
       },{
         day: 22,
         caption: 'Don\'t open the presents until Christmas!',
-        img: 'https://i.pinimg.com/564x/a8/c8/12/a8c812ba32024567c237ad5933636547.jpg'
+        img: `${dog22}`
       },{
         day: 23,
         caption: 'Finish your Christmas shopping',
-        img: 'https://i.pinimg.com/564x/7d/6e/e1/7d6ee1d29919b0503f74d7f50f233723.jpg'
+        img: `${dog23}`
       },{
         day: 10,
         caption: 'Choose a Christmas party outfit',
-        img: 'https://www.rover.com/blog/wp-content/uploads/2017/12/snowmandog-960x540.jpg'
+        img: `${dog10}`
       },{
         day: 15,
         caption: 'Wrap your Christmas presents',
-        img: 'https://f6j9p4k6.stackpathcdn.com/wp-content/uploads/2016/12/b0277d9f038439fd1f5a4522fc2de2e3-photography-ideas-for-dogs-pet-photography-ideas.jpg'
+        img: `${dog15}`
       },{
         day: 2,
         caption: 'Put up the Christmas lights',
-        img: 'https://i.pinimg.com/564x/4f/c6/86/4fc686ec00240e1d842565bb5c3c8be9.jpg'
+        img: `${dog2}`
       },{
         day: 6,
         caption: 'Dig out your Christmas jumper',
-        img: 'https://i.pinimg.com/564x/df/9b/61/df9b619fa6b58910ede5431ac50c87de.jpg'
+        img: `${dog6}`
       },{
         day: 9,
         caption: 'Get a Christmas tree',
-        img: 'https://i.pinimg.com/564x/a3/76/64/a37664414da3b0e37f9bfc35311bd133.jpg'
+        img: `${dog9}`
       },{
         day: 14,
         caption: 'Get a pre Christmas nap in',
-        img: 'https://3milliondogs.com/blog-assets-two/2016/11/desktop-pictures-of-christmas-dogs-wallpaper.jpg'
+        img: `${dog14}`
       },{
         day: 11,
         caption: 'Do some Christmas baking',
-        img: 'https://www.rover.com/blog/wp-content/uploads/2015/11/fruitnut.jpg'
+        img: `${dog11}`
       },{
         day: 13,
         caption: 'Hope to meet your love beneath the misletoe',
-        img: 'https://www.mediastorehouse.com/p/172/dalmatian-dog-x2-mistletoe-647221.jpg'
+        img: `${dog13}`
       },{
         day: 12,
         caption: 'Go to a Nativity',
-        img: 'https://pbs.twimg.com/media/B5H8urzIUAAoAXj.jpg'
+        img: `${dog12}`
       }
     ];
 
@@ -169,6 +213,7 @@ class App extends Component {
 
         <div className="calendar">
           {advent.map((day, i) =>
+
 
               <Card
                 day={day.day}
